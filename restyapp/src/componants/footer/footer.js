090a514a89e3d9c1a8ffb3408e3bd3e6cfdbece6
idreesalmasri <idrees.massri95@gmail.com>
2022-05-16 19:44:58 +0300

    phase-3 done

---
+++ restyapp/src/componants/footer/footer.js
@@ -0,0 +1,12 @@
+
+import './footer.css';
+
+const Footer = () => {
+    return(
+    <div className="footer">
+    <p>Copyright © 2022 lab-26</p>
+  </div>
+  )
+  
+    }
+export default Footer;
---
+++ restyapp/src/componants/header/Header.js
@@ -0,0 +1,19 @@
+import './header.css';
+
+const Header = () => {
+    return(
+      <>
+  <div className="header">
+    <h1 id='h'>RESTy</h1>
+  </div>
+  <nav className='nav'>
+            <ul className='ul'> 
+            <li><a className='li' href="/">Home</a></li>
+            <li><a className='li' href="/History">History</a></li>
+            <li><a className='li' href="/Help">Help</a></li>
+            </ul>
+        </nav>
+  </>
+    )
+}
+export default Header;
